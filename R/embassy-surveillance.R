4c1f6798678933a4f1cc6279eeb23e488f6eb0d7
Tu Hu <UYHDK@leo-pharma.com>
2020-12-02 17:18:54 +0100

    Add read more

--- R/embassy-surveillance.R
+++ R/embassy-surveillance.R
@@ -13,21 +13,22 @@ extract_content <- function(url = ...){
 }
 html <- xml2::read_html("https://www.fmprc.gov.cn/ce/cedk/chn/lsfw/")
 
-page <- tibble(title = rvest::html_nodes(html, css = ".newsList a") %>% rvest::html_text() %>% 
+page <- tibble(Title = rvest::html_nodes(html, css = ".newsList a") %>% rvest::html_text() %>% 
                  as.character() %>% str_replace_all(BLANK, ""),
-               time = rvest::html_nodes(html,css = ".newsList span") %>% rvest::html_text() %>% 
+               Time = rvest::html_nodes(html,css = ".newsList span") %>% rvest::html_text() %>% 
                  as.character() %>% str_remove_all("\\(|\\)") %>% as.Date(),
                url = rvest::html_nodes(html, css = ".newsList a") %>% as.character() %>% 
                  str_extract('(?<=href=")[:graph:]{1,}(?=\")')) %>% 
   mutate(url = url %>% str_replace(START %R% DOT, "https://www.fmprc.gov.cn/ce/cedk/chn/lsfw")) %>% 
-  dplyr::arrange(desc(time)) 
+  dplyr::arrange(desc(Time)) 
 
 content <- page$url %>% map_chr(extract_content)
-  mutate(content = extract_content(url))
 
-
-
-page_content_preview <- page_content %>% mutate(content = content %>% str_remove_all("TRUE") %>% str_sub(1, 120))
-
-test <- knitr::kable(page_content_preview, format = "pipe")
+page_content_preview <- 
+  page %>% 
+  mutate(Content_preview = paste0("[",content %>% str_remove_all("TRUE") %>% str_sub(1, 120), " ... (Read More)]","(", url,")")) %>% 
+  select(-url)
+test <- knitr::kable(page_content_preview , format = "pipe")
 write(test, "README.md")
+
+
