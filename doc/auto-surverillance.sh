bd2fad66afd1973d8a9fb15cf3513dbe41a35a01
Tu Hu <UYHDK@leo-pharma.com>
2021-03-06 18:18:16 +0100

    update
    6 MAR

--- doc/auto-surverillance.sh
+++ doc/auto-surverillance.sh
@@ -2,7 +2,7 @@
 cd /Users/tuhu/Projects/chinaembassy-dk-survillance
 Rscript R/embassy-surveillance-CN.R
 Rscript R/embassy-surveillance-EN.R
-git add README.md
-git add EN/README.md
-git commit -m update
-git push
+#git add README.md
+#git add EN/README.md
+#git commit -m update
+#git push
